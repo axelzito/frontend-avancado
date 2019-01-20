6b80cf7ec1cbec583ddbbd1c8734882d2665df1e
Ítalo Tavares <gasteba@gmail.com>
2019-01-19 22:34:25 -0200

    rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //home
+  {
+    path: '',
+    component: HomeComponent
+  },
+  //Products
+  {
+    path: 'product',
+    component: ProductComponent
+  },
+  //Cart
+  {
+    path: 'cart',
+    component: CartComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
