e57715fc21efcb8174e3c394ed7525be5b3ea600
thiagocgp <thiagocgp@gmail.com>
2019-01-22 17:08:32 -0200

    Ajustando a página de produto e o service

--- src/app/storage.service.ts
+++ src/app/storage.service.ts
@@ -8,24 +8,11 @@ import { Observable } from 'rxjs';
 })
 export class StorageService {
   private _url: string = "/assets/data/books.json";
-  private _product:IStorage = null;
-
+  
   constructor(private http: HttpClient) { }
 
   getStorage():Observable<IStorage[]>{
     return this.http.get<IStorage[]>(this._url);
   }
 
-  getProductById(id:number):IStorage{
-    this.getStorage().subscribe(data => {
-      for(var i = 0; i < data.length; i++) {
-        if(data[i].id == id){
-          this._product.title = data[i].title;
-          return;
-        }
-      }
-    });
-    console.log(this._product.title);
-    return this._product;
-  }
 }
