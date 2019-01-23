8cbc61ba486ed7911a833330e85f5dfc7edfaaa7
axelzito <axel.ataides@gmail.com>
2019-01-22 23:17:28 -0200

    melhorando tela inicial

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,9 +27,15 @@ export class HomeComponent {
 
   constructor() {
     this.items = [
-      { name: 'assets/data/img/001.jpg' },
-      { name: 'assets/data/img/002.jpg' },
-      { name: 'assets/data/img/003.jpg' },
+      { name: 'assets/data/img-mini/001.jpg' },
+      { name: 'assets/data/img-mini/002.jpg' },
+      { name: 'assets/data/img-mini/003.jpg' },
+      { name: 'assets/data/img-mini/001.jpg' },
+      { name: 'assets/data/img-mini/002.jpg' },
+      { name: 'assets/data/img-mini/003.jpg' },
+      { name: 'assets/data/img-mini/001.jpg' },
+      { name: 'assets/data/img-mini/002.jpg' },
+      { name: 'assets/data/img-mini/003.jpg' },
       // { name: 'assets/data/img/004.jpg' },
       // { name: 'assets/data/img/005.jpg' },
       // { name: 'assets/data/img/006.jpg' },
