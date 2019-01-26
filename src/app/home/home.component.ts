9785103d0b1800f1a41a230bf0ea2b3fcde99337
thiagocgp <thiagocgp@gmail.com>
2019-01-26 01:07:33 -0200

    Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,28 +8,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  items: Array<any> = [];
+  public items = [];
+  public itemsInfantil = [];
+  public itemsMaisVendidos = [];
 
-  constructor() {
-    this.items = [
-      { name: 'assets/data/img-mini/001.jpg' },
-      { name: 'assets/data/img-mini/002.jpg' },
-      { name: 'assets/data/img-mini/003.jpg' },
-      { name: 'assets/data/img-mini/001.jpg' },
-      { name: 'assets/data/img-mini/002.jpg' },
-      { name: 'assets/data/img-mini/003.jpg' },
-      { name: 'assets/data/img-mini/001.jpg' },
-      { name: 'assets/data/img-mini/002.jpg' },
-      { name: 'assets/data/img-mini/003.jpg' },
-      // { name: 'assets/data/img/004.jpg' },
-      // { name: 'assets/data/img/005.jpg' },
-      // { name: 'assets/data/img/006.jpg' },
-      // { name: 'assets/data/img/001.jpg' },
-      // { name: 'assets/data/img/002.jpg' },
-      // { name: 'assets/data/img/003.jpg' },
-      // { name: 'assets/data/img/004.jpg' },
-      // { name: 'assets/data/img/005.jpg' },
-      // { name: 'assets/data/img/006.jpg' },
-    ];
+  constructor(private _storageService: StorageService, private _router: Router) { }
+
+  ngOnInit() {
+    this.items = this.listAllProducts();
+    this.itemsInfantil = this.listByCategory('Infantil');
+    this.itemsMaisVendidos = this.listBestSellers();
   }
+
+  listByCategory(category:string):any[]{
+    let product = [];
+    this._storageService.getStorage().subscribe(data => {
+      for(var i = 0; i < data.length; i++) {
+        if(data[i].category === category){
+          product.push({
+            title: data[i].title,
+            imgPath: data[i].imgPath,
+            id: data[i].id,
+            originalPrice: data[i].originalPrice,
+            price: data[i].price
+          });
+        }
+      }
+    });
+    return product;
+  }
+
+  listBestSellers():any[]{
+    let product = [];
+    this._storageService.getStorage().subscribe(data => {
+      for(var i = 0; i < data.length; i++) {
+        if(data[i].bestSeller === 'true'){
+          product.push({
+            title: data[i].title,
+            imgPath: data[i].imgPath,
+            id: data[i].id,
+            originalPrice: data[i].originalPrice,
+            price: data[i].price
+          });
+        }
+      }
+    });
+    return product;
+  }
+
+  listAllProducts():any[]{
+    let product = [];
+    this._storageService.getStorage().subscribe(data => {
+      for(var i = 0; i < data.length; i++) {
+        product.push({
+          title: data[i].title,
+          imgPath: data[i].imgPath,
+          id: data[i].id,
+          originalPrice: data[i].originalPrice,
+          price: data[i].price
+        });
+      }
+    });
+    return product;
+  }
+
+  navigateToProduct(id){
+    this._router.navigate(['/product', id]);
+  }
+  
 }
